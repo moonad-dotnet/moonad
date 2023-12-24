1d921402fa7f73dde6079092834d4fd8bb028c3b
William Santos <me@wsantos.dev>
2023-12-24 18:24:02 -0300

    fix(tests): Result<T> failure test

--- src/Result/ResultT.cs
+++ src/Result/ResultT.cs
@@ -22,6 +22,9 @@
         public static implicit operator Result<T>(T value) =>
             new(value);
 
+        public static implicit operator T(Result<T> result) =>
+            result.Value;
+
         public static implicit operator bool(Result<T> result) =>
             result.IsOk;
 
