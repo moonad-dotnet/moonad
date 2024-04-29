c7e31323f4d35bde7fa760de9b6ac0807e4906da
William Santos <me@wsantos.dev>
2024-04-29 01:11:06 -0300

    feat(Result): adds TError version

---
+++ src/Result/ResultE.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Moonad
+{
+    public readonly struct Result<TError>: IEquatable<Result<TError>> where TError : notnull
+    {
+        public readonly TError _errorValue;
+
+        public readonly TError ErrorValue =>
+            !IsOk
+                ? _errorValue
+                : throw new ErrorValueException();
+
+        public readonly bool IsOk;
+        public readonly bool IsError => !IsOk;
+
+        private Result(bool isOk, TError resultError = default!) =>
+            (IsOk, _errorValue) = (isOk, resultError);
+
+        public static Result<TError> Ok() =>
+            new(true);
+
+        public static Result<TError> Error(TError error) =>
+            new(false, error);
+
+        public static implicit operator Result<TError>(bool isOk) =>
+            isOk 
+                ? new(isOk) 
+                : throw new ErrorValueException("A Result<TError> must be initialized with an error");
+        
+        public static implicit operator Result<TError>(TError error) =>
+            Error(error);
+
+        public static implicit operator TError(Result<TError> result) =>
+            result.ErrorValue;
+
+        public static implicit operator bool(Result<TError> result) =>
+            result.IsOk;
+
+        public static bool operator ==(Result<TError> left, Result<TError> right) =>
+            left.Equals(right);
+
+        public static bool operator !=(Result<TError> left, Result<TError> right) =>
+            !(left == right);
+
+        public bool Equals(Result<TError> other)
+        {
+            if (IsOk && other.IsOk)
+                return true;
+
+            return false;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null || obj is not Result<TError> other)
+                return false;
+
+            return Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return IsOk 
+                ? base.GetHashCode()
+                : ErrorValue.GetHashCode();
+        }
+    }
+}
---
+++ test/Result/ResultE.Tests.cs
@@ -0,0 +1,51 @@
+﻿namespace Moonad.Tests.Results
+{
+    public class ResultETests
+    {
+        [Fact]
+        public void IsOk()
+        {
+            //Act
+            var result = Result<string>.Ok();
+
+            //Assert
+            Assert.True(result);
+            Assert.Throws<ErrorValueException>(() => result.ErrorValue);
+        }
+
+        [Fact]
+        public void ImplicitOk()
+        {
+            //Act
+            Result<IError> result = true;
+
+            //Assert
+            Assert.True(result);
+            Assert.True(result.IsOk);
+        }
+
+        [Fact]
+        public void IsError()
+        {
+            //Act
+            Result<IError> result = new DummyError();
+
+            //Assert
+            Assert.False(result);
+            Assert.IsType<DummyError>(result.ErrorValue);
+        }
+
+        [Fact]
+        public void ImplicitError()
+        {
+            //Arrange
+            static void action() { Result<IError> result = false; }
+
+            //Assert
+            Assert.Throws<ErrorValueException>(action);
+        }
+    }
+
+    public interface IError { }
+    public readonly struct DummyError : IError { }
+}
